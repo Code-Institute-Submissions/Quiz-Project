8761ae84b0fe80d4f15682b90be2b3a1c10cea7b
Tom McNally <tom_mcnally17@hotmail.co.uk>
2018-05-19 16:29:16 +0000

    added routes and templates and data

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import json
 from flask import Flask, redirect, render_template, request, flash, url_for
 
 app = Flask(__name__)
+app.secret_key = "some_secert"
 
 def get_questions():
     
@@ -49,6 +50,74 @@ def get_choices(question_number):
     choices = questions[question_number]["choices"]
     
     return choices
+
+def wrong_guess(username, guess, choices, wrong_guesses):
+    
+    if guess in choices:
+        wrong_guesses.append(guess)
+    else:
+        flash("Invalid Guess, Try Again!")
+
+    return wrong_guesses
+
+def set_scoreboard(username, question_number, score):
+    
+    with open("data/{0}.txt".format(username), "a") as user_file:
+        user_file.writelines("({0}) Question {1}: {2}\n".format(
+            username,
+            (question_number + 1),
+            score))
+    
+@app.route("/", methods=["GET", "POST"])
+def index():
+    
+    # Quiz Homepage with username entry
+    question_number = get_question_number()
+    
+    if request.method == "POST":
+        username = request.form["username"]
+        with open("data/users.txt", "a") as users:
+            users.writelines(username + "\n")
+        return redirect(url_for("quiz", username=username, question_number=question_number))
+        
+    return render_template("index.html")
+    
+@app.route("/<username>/<int:question_number>", methods=["GET", "POST"])
+def quiz(username, question_number):
+    
+    question = get_question(question_number)
+    answer = get_right_answer(question_number)
+    choices = get_choices(question_number)
+    wrong_guesses = []
+   
+   
+    if request.method == "POST":
+        guess = request.form["guess"].title()
+        if guess == answer:
+            score = 1
+            set_scoreboard(username, question_number, score)
+            if question_number == 2:
+                return redirect("/leaderboard")
+            else:
+                return redirect(url_for("quiz", username=username, question_number=question_number+1))
+        elif guess != answer:
+           
+            wrong_guess(username, guess, choices, wrong_guesses)
+        else:
+            score = 0
+            set_scoreboard(username, question_number, score)
+            if question_number == 2:
+                return redirect("/leaderboard")
+            else:
+                return redirect(url_for("quiz", username=username, question_number=question_number+1))
+        
+            
+    return render_template("quiz.html", username=username, question=question, wrong_guesses=wrong_guesses)
+    
+@app.route("/leaderboard")
+def leaderboard():
+    
+    return render_template("leaderboard.html")
 
 
 
