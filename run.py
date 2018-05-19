b8804a1042154b98192675c0bde8155d52b5612c
Tom McNally <tom_mcnally17@hotmail.co.uk>
2018-05-19 16:12:39 +0000

    added tests

--- run.py
+++ run.py
@@ -4,6 +4,52 @@ from flask import Flask, redirect, render_template, request, flash, url_for
 
 app = Flask(__name__)
 
+def get_questions():
+    
+    questions = []
+    with open("data/questions.json", "r") as quiz_data:
+        questions = json.load(quiz_data)
+    return questions
+    
+def get_question_number():
+    
+    questions = get_questions()
+    question_number = questions[0]["index"]
+    
+    return question_number
+    
+def number_of_questions():
+    
+    questions = get_questions()
+    number_of_question = len(questions)
+    
+    return number_of_question
+    
+def get_question(question_number):
+    
+    question = []
+    with open("data/questions.json", "r") as quiz_data:
+        questions = json.load(quiz_data)
+        for obj in questions:
+            if obj["index"] == question_number:
+                question = obj
+            
+    return question
+    
+def get_right_answer(question_number):
+    
+    questions = get_questions()
+    answer = questions[question_number]["answer"]
+    
+    return answer
+    
+def get_choices(question_number):
+    
+    questions = get_questions()
+    choices = questions[question_number]["choices"]
+    
+    return choices
+
 
 
 if __name__ == "__main__":
--- test_run.py
+++ test_run.py
@@ -5,3 +5,69 @@ import run
 class test_quiz(unittest.TestCase):
     
     # Test suite
+    
+    def test_get_questions(self):
+        
+        # Test to get questions from question file
+        
+        questions = run.get_questions()
+        self.assertEqual((questions[0]), {
+            "index": 0,
+            "number": 1,
+            "question": "What is the capital of the UK?",
+            "choices": ["Paris", "London", "Berlin", "Oslo"],
+            "answer": "London"
+            })
+        self.assertNotIn("question 4", questions)
+        
+    def test_get_question_number(self):
+        
+        # Test to get question number to start of quiz
+        
+        question_number = run.get_question_number()
+        
+        self.assertEqual(question_number, 0)
+        
+    def test_get_question(self):
+        
+        # Test that get question with the question number will return correct question
+        
+        question = run.get_question(0)
+        
+        self.assertEqual(question, {
+            "index": 0,
+            "number": 1,
+            "question": "What is the capital of the UK?",
+            "choices": ["Paris", "London", "Berlin", "Oslo"],
+            "answer": "London"
+            })
+    
+    def test_get_right_answer(self):
+        
+        # Test to get the right answer to question using question number
+        
+        answer = run.get_right_answer(0)
+        answer_1 = run.get_right_answer(1)
+        
+        self.assertEqual(answer, "London")
+        self.assertNotEqual(answer_1, "London")
+        
+        
+    def test_get_choices(self):
+        
+        # Test to get the choices to question using question number
+        
+        choices = run.get_choices(0)
+        
+        self.assertEqual(choices, ["Paris", "London", "Berlin", "Oslo"])
+        self.assertNotEqual(choices, ["Blue", "Red", "Green", "Yellow"])
+        
+    
+    def test_right_answer(self):
+        
+        # Test that right answer to question will increase score by 1
+        
+        answer = run.get_right_answer(0)
+        guess = "London"
+        
+        self.assertEqual(answer, guess)
