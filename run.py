66d2a908c51e722d3007071b79a255a866c956d9
Tom McNally <tom_mcnally17@hotmail.co.uk>
2018-06-06 19:35:49 +0000

    updated some functions and added a remove temp file

--- run.py
+++ run.py
@@ -1,18 +1,20 @@
 import os
 import json
 from flask import Flask, redirect, render_template, request, flash, url_for
-from operator import itemgetter
 
 app = Flask(__name__)
 app.secret_key = "some_secert"
 
 def add_user(username):
     
+    # Adds "Username" to Users
     
     with open("data/users.txt", "a") as users:
         users.write("{0}\n".format(username))
 
-def duplicate_user():
+def get_userlist():
+    
+    # Stops "Username" being used twice
     
     user_list = []
     
@@ -20,7 +22,6 @@ def duplicate_user():
         user_list = [user.strip("\n") for user in users]
         
     return user_list
-        
 
 def get_questions():
     
@@ -80,25 +81,41 @@ def get_choices(question_number):
     
     return choices
     
+def wrong_guesses_open(username):
+    
+    # Opens Wrong Guesses File and Wipes for next Question
+    
+    with open("data/{0}_guesses.txt".format(username), "w") as guesses:
+        guesses.write("")
+    
 def wrong_guess(username, guess, choices, wrong_guesses):
     
     # Adds incorrect guess to wrong guesses or shows alerts user it was invalid 
     
     if guess in choices:
-        wrong_guesses.append(guess)
+        with open("data/{0}_guesses.txt".format(username), "a") as guesses:
+            guesses.write("{0}, ".format(guess))
     else:
         flash("Invalid Guess, Try Again!")
         
 def get_wrong_guesses(username):
     
+    # Gets Wrong Guess for User 
+    
     with open("data/{0}_guesses.txt".format(username), "r") as guesses:
         wrong_guesses = guesses.read()
-            
-    return wrong_guesses
+        
+        return wrong_guesses
+        
+def remove_wrong_guesses(username):
+    
+    # Removes wrong guesses file for user
+    
+    os.remove("data/{0}_guesses.txt".format(username))
 
 def add_to_scoreboard(username, score):
     
-    # Adds users score to scoreboard file
+    # Adds Users score to scoreboard file
     
     with open("data/scoreboard.json", "r+") as scoreboard:
         users = json.load(scoreboard)
@@ -114,13 +131,14 @@ def add_to_scoreboard(username, score):
             
 def get_scoreboard():
     
+    # Gets all scores and sorts them into highest first but Only Top 5
+    
     with open("data/scoreboard.json", "r") as scores:
         all_scores = json.load(scores)
         
     top_scores = sorted(all_scores, key=lambda x:x["score"], reverse=True)[:5]
     
     return top_scores
-    
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -128,7 +146,7 @@ def index():
     # Quiz Homepage with username entry and quiz start
     
     question_number = get_question_number()
-    users = duplicate_user()
+    users = get_userlist()
     
     if request.method == "POST":
         username = request.form["username"]
@@ -136,6 +154,7 @@ def index():
             flash("The username '{0}' is unavailable, Try Again!".format(username))
         else:
             add_user(username)
+            wrong_guesses_open(username)
             return redirect(url_for("quiz", username=username,
                                             question_number=question_number,
                                             score=0,
@@ -152,7 +171,7 @@ def quiz(username, question_number, score, guesses):
     answer = get_right_answer(question_number)
     choices = get_choices(question_number)
     no_of_questions = number_of_questions()
-    wrong_guesses = []
+    wrong_guesses = get_wrong_guesses(username)
    
     if request.method == "POST":
         if guesses != 1:
@@ -163,14 +182,16 @@ def quiz(username, question_number, score, guesses):
                 return redirect(url_for("quiz", username=username,
                                                 question_number=question_number,
                                                 score=score,
-                                                guesses=guesses))
+                                                guesses=guesses,
+                                                wrong_guesses=wrong_guesses))
             else:
                 score += 1
                 if question_number == (no_of_questions - 1):
                     add_to_scoreboard(username, score)
-                    return redirect(url_for("leaderboard", username=username,
+                    return redirect(url_for("quiz_end", username=username,
                                                             score=score))
                 else:
+                    wrong_guesses_open(username)
                     return redirect(url_for("quiz", username=username,
                                                     score=score,
                                                     question_number=question_number+1,
@@ -178,9 +199,10 @@ def quiz(username, question_number, score, guesses):
         else:
             if question_number == (no_of_questions - 1):
                 add_to_scoreboard(username, score)
-                return redirect(url_for("leaderboard", username=username,
+                return redirect(url_for("quiz_end", username=username,
                                                         score=score))
             else:
+                wrong_guesses_open(username)
                 return redirect(url_for("quiz", username=username,
                                                 score=score,
                                                 question_number=question_number+1,
@@ -189,21 +211,29 @@ def quiz(username, question_number, score, guesses):
     return render_template("quiz.html", username=username,
                                         question=question,
                                         guesses=guesses,
-                                        wrong_guesses=wrong_guesses)
+                                        wrong_guesses=wrong_guesses,
+                                        no_of_questions=no_of_questions)
+
+@app.route("/quiz_end/<username>/<int:score>")
+def quiz_end(username, score):
     
-@app.route("/leaderboard/<username>/<int:score>")
-def leaderboard(username, score):
-    
-    # Leaderboard shows Top 5 scores and displays Users score after completing Quiz
+    # End of Quiz with Users score
     
     no_of_questions = number_of_questions()
+    remove_wrong_guesses(username)
+    
+    return render_template("quiz_end.html", username=username,
+                                            score=score,
+                                            no_of_questions=no_of_questions)
+
+@app.route("/leaderboard")
+def leaderboard():
+    
+    # Leaderboard shows Top 5 scores
+    
     top_scores = get_scoreboard()
     
-    
-    return render_template("leaderboard.html", username=username,
-                                            score=score,
-                                            no_of_questions=no_of_questions,
-                                            leaderboard=top_scores)
+    return render_template("leaderboard.html", leaderboard=top_scores)
     
 
 if __name__ == "__main__":
