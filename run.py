145aeef2abc0eacda9ea5319dd7bda2912ed29d7
Tom McNally <tom_mcnally17@hotmail.co.uk>
2019-02-22 18:15:30 +0000

    changed comments to docstrings and formatted templates

--- run.py
+++ run.py
@@ -2,20 +2,21 @@ import os
 import json
 from flask import Flask, redirect, render_template, request, flash, url_for
 
+
 app = Flask(__name__)
 app.secret_key = "some_secert"
 
 def add_user(username):
-    
-    # Adds "Username" to Users
-    
+    """
+    Adds "Username" to Users
+    """
     with open("data/users.txt", "a") as users:
         users.write("{0}\n".format(username))
 
 def get_userlist():
-    
-    # Stops "Username" being used twice
-    
+    """
+    Stops "Username" being used twice
+    """
     user_list = []
     
     with open("data/users.txt", "r") as users:
@@ -24,36 +25,36 @@ def get_userlist():
     return user_list
 
 def get_questions():
-    
-    # Gets "Questions" from json file
-    
+    """
+    Gets "Questions" from json file
+    """
     questions = []
     with open("data/questions.json", "r") as quiz_data:
         questions = json.load(quiz_data)
     return questions
     
 def get_question_number():
-    
-    # Gets "Question Number" to start of quiz
-    
+    """
+    Gets "Question Number" to start of quiz
+    """
     questions = get_questions()
     question_number = questions[0]["index"]
     
     return question_number
     
 def number_of_questions():
-    
-    # Gets "Number of Questions" for end of quiz and "score out of"
-    
+    """
+    Gets "Number of Questions" for end of quiz and "score out of"
+    """
     questions = get_questions()
     number_of_question = len(questions)
     
     return number_of_question
     
 def get_question(question_number):
-    
-    # Gets "Question" for quiz using "Question Number"
-    
+    """
+    Gets "Question" for quiz using "Question Number"
+    """
     questions = get_questions()
     for obj in questions:
         if obj["index"] == question_number:
@@ -62,34 +63,34 @@ def get_question(question_number):
     return question
     
 def get_right_answer(question_number):
-    
-    # Gets correct answer to "Question"
-     
+    """
+    Gets correct answer to "Question"
+     """
     questions = get_questions()
     answer = questions[question_number]["answer"]
     
     return answer
     
 def get_choices(question_number):
-    
-    # Gets available "Choices" for "Question"
-    
+    """
+    Gets available "Choices" for "Question"
+    """
     questions = get_questions()
     choices = questions[question_number]["choices"]
     
     return choices
     
 def wrong_guesses_open(username):
-    
-    # Opens Wrong Guesses File and Wipes for next Question
-    
+    """
+    Opens Wrong Guesses File and Wipes for next Question
+    """
     with open("data/{0}_guesses.txt".format(username), "w") as guesses:
         guesses.write("")
     
 def wrong_guess(username, guess, choices, wrong_guesses):
-    
-    # Adds incorrect guess to wrong guesses or shows alerts user it was invalid 
-    
+    """
+    Adds incorrect guess to wrong guesses or shows alerts user it was invalid 
+    """
     if guess in choices:
         with open("data/{0}_guesses.txt".format(username), "a") as guesses:
             guesses.write("{0}, ".format(guess))
@@ -97,52 +98,50 @@ def wrong_guess(username, guess, choices, wrong_guesses):
         flash("Invalid Guess, Try Again!")
         
 def get_wrong_guesses(username):
-    
-    # Gets Wrong Guess for User 
-    
+    """
+    Gets Wrong Guess for User 
+    """
     with open("data/{0}_guesses.txt".format(username), "r") as guesses:
         wrong_guesses = guesses.read()
         
         return wrong_guesses
         
 def remove_wrong_guesses(username):
-    
-    # Removes wrong guesses file for user
-    
+    """
+    Removes wrong guesses file for user
+    """
     os.remove("data/{0}_guesses.txt".format(username))
 
 def add_to_scoreboard(username, score):
-    
-    # Adds Users score to scoreboard file
-    
+    """
+    Adds Users score to scoreboard file
+    """
     with open("data/scoreboard.json", "r+") as scoreboard:
         users = json.load(scoreboard)
         
     user = {}
     user["username"] = username
     user["score"] = score
-    
     users.append(user)
     
     with open("data/scoreboard.json", "r+") as scoreboard:
         json.dump(users, scoreboard, indent=4)
             
 def get_scoreboard():
-    
-    # Gets all scores and sorts them into highest first but Only Top 5
-    
+    """
+    Gets all scores and sorts them into highest first but Only Top 5
+    """
     with open("data/scoreboard.json", "r") as scores:
         all_scores = json.load(scores)
         
     top_scores = sorted(all_scores, key=lambda x:x["score"], reverse=True)[:5]
-    
     return top_scores
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    
-    # Quiz Homepage with username entry and quiz start
-    
+    """
+    Quiz Homepage with username entry and quiz start
+    """
     question_number = get_question_number()
     users = get_userlist()
     
@@ -162,9 +161,9 @@ def index():
     
 @app.route("/<username>/<int:question_number>/<int:score>/<int:guesses>", methods=["GET", "POST"])
 def quiz(username, question_number, score, guesses):
-    
-    # Playing Quiz steps through questions after a correct guess or 3 incorrect guesses
-    
+    """
+    Playing Quiz steps through questions after a correct guess or 3 incorrect guesses
+    """
     question = get_question(question_number)
     answer = get_right_answer(question_number)
     choices = get_choices(question_number)
@@ -240,9 +239,9 @@ def quiz(username, question_number, score, guesses):
 
 @app.route("/quiz_end/<username>/<int:score>")
 def quiz_end(username, score):
-    
-    # End of Quiz with Users score
-    
+    """
+    End of Quiz with Users score
+    """
     no_of_questions = number_of_questions()
     remove_wrong_guesses(username)
     
@@ -252,15 +251,13 @@ def quiz_end(username, score):
 
 @app.route("/leaderboard")
 def leaderboard():
-    
-    # Leaderboard shows Top 5 scores
-    
+    """
+    Leaderboard shows Top 5 scores
+    """
     top_scores = get_scoreboard()
     
     return render_template("leaderboard.html", leaderboard=top_scores)
     
 
 if __name__ == "__main__":
-    app.run(host= os.environ.get("IP"),
-    port= int(os.environ.get("PORT")),
-    debug= True)
+    app.run(host=os.environ.get("IP"), port=int(os.environ.get("PORT")))
--- tests/test_run.py
+++ tests/test_run.py
@@ -2,22 +2,22 @@ import unittest
 import run
 
 class test_quiz(unittest.TestCase):
-    
-    # Test suite
-    
+    """
+    Test suite
+    """
     def test_username(self):
-        
-        # Test that the username is added to userlist
-        
+        """
+        Test that the username is added to userlist
+        """
         run.add_user("testSuite")
         userlist = run.get_userlist()
         
         self.assertIn("testSuite", userlist)
         
     def test_get_questions(self):
-        
-        # Test to get questions from question file
-        
+        """
+        Test to get questions from question file
+        """
         questions = run.get_questions()
         self.assertEqual((questions[0]), {
            "index": 0,
@@ -28,17 +28,17 @@ class test_quiz(unittest.TestCase):
             })
         
     def test_get_question_number(self):
-        
-        # Test to get question number to start of quiz
-        
+        """
+        Test to get question number to start of quiz
+        """
         question_number = run.get_question_number()
         
         self.assertEqual(question_number, 0)
         
     def test_get_question(self):
-        
-        # Test that get question with the question number will return correct question
-        
+        """
+        Test that get question with the question number will return correct question
+        """
         question = run.get_question(0)
         
         self.assertEqual(question, {
@@ -50,29 +50,28 @@ class test_quiz(unittest.TestCase):
             })
     
     def test_get_right_answer(self):
-        
-        # Test to get the right answer to question using question number
-        
+        """
+        Test to get the right answer to question using question number
+        """
         answer = run.get_right_answer(0)
         answer_1 = run.get_right_answer(1)
         
         self.assertEqual(answer, "Vienna")
         self.assertNotEqual(answer_1, "Germany")
         
-        
     def test_get_choices(self):
-        
-        # Test to get the choices to question using question number
-        
+        """
+        Test to get the choices to question using question number
+        """
         choices = run.get_choices(0)
         
         self.assertEqual(choices, ["Paris", "Vienna", "Prague", "Venice"])
         self.assertNotEqual(choices, ["Germany", "Belgium", "Croatia", "Switzerland"])
         
     def test_scoreboard(self):
-        
-        # Test that the scoreboard is correct
-        
+        """
+        Test that the scoreboard is correct
+        """
         run.add_to_scoreboard("testSuite", 12)
         leaderboard = run.get_scoreboard()
         
